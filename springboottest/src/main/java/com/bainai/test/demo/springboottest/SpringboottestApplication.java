a065cd73bd68abcfaaa1d412bc093183137cdc4f
lijiaxing <1216240145@qq.ocm>
2020-04-17 09:11:20 +0800

    test

---
+++ springboottest/src/main/java/com/bainai/test/demo/springboottest/SpringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.bainai.test.demo.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboottestApplication.class, args);
+    }
+
+}
